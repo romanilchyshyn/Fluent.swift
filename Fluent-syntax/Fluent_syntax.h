c5a0224aab6f7fa3f927ca14fbe1c9acacb43429
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-08-11 09:40:44 +0300

    Add 'Fluent-syntax' target.

---
+++ Fluent-syntax/Fluent_syntax.h
@@ -0,0 +1,19 @@
+//
+//  Fluent_syntax.h
+//  Fluent-syntax
+//
+//  Created by  Roman Ilchyshyn on 11.08.2020.
+//  Copyright © 2020  Roman Ilchyshyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fluent_syntax.
+FOUNDATION_EXPORT double Fluent_syntaxVersionNumber;
+
+//! Project version string for Fluent_syntax.
+FOUNDATION_EXPORT const unsigned char Fluent_syntaxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fluent_syntax/PublicHeader.h>
+
+
