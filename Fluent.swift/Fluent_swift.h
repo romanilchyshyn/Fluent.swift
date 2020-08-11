96b61336d38839bfe3f3cec7d01b21612e48079c
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-08-11 09:36:15 +0300

    Initial Commit

---
+++ Fluent.swift/Fluent_swift.h
@@ -0,0 +1,19 @@
+//
+//  Fluent_swift.h
+//  Fluent.swift
+//
+//  Created by  Roman Ilchyshyn on 11.08.2020.
+//  Copyright © 2020  Roman Ilchyshyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fluent_swift.
+FOUNDATION_EXPORT double Fluent_swiftVersionNumber;
+
+//! Project version string for Fluent_swift.
+FOUNDATION_EXPORT const unsigned char Fluent_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fluent_swift/PublicHeader.h>
+
+
