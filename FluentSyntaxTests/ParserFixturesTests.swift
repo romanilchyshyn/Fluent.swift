7529d8cd6ed991b416d19906ab39e8b195b9f584
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-08-20 14:04:40 +0300

    Parser tests (failing).

--- FluentSyntaxTests/ParserFixturesTests.swift
+++ FluentSyntaxTests/ParserFixturesTests.swift
@@ -13,49 +13,55 @@ final class ParserFixturesTests: XCTestCase {
 
     func test_ParseFixtures() {
         let bundle = Bundle(for: type(of: self))
-        let ftlUrls = bundle.urls(forResourcesWithExtension: "ftl", subdirectory: "fixtures")!
-        
-        for url in ftlUrls {
-            print("START: \(url.lastPathComponent)")
-            
-            let s = try! String(contentsOf: url, encoding: .utf8)
-            
-            let r = parse(source: s)
-        
-            if case .success = r {
-                print("   OK: \(url.lastPathComponent)")
-            } else {
-                print(" FAIL: \(url.lastPathComponent)")
-            }
-            
+        let urls = bundle.urls(forResourcesWithExtension: "ftl", subdirectory: "fixtures")!
+
+        for url in urls.sorted(by: { $0.path < $1.path }) {
+            Self.testFixture(ftlUrl: url)
         }
-        
-    }
-    
-    func test_ParseFixture() {
-        let fixture = "tab"
-        
-        print("START: \(fixture)")
-        
-        let bundle = Bundle(for: type(of: self))
-        let url = bundle.url(forResource: fixture, withExtension: "ftl", subdirectory: "fixtures")!
-        let s = try! String(contentsOf: url, encoding: .utf8)
-        
-        let r = parse(source: s)
-        
-        if case .success = r {
-            print("OK: \(fixture)")
-        } else {
-            print("FAIL: \(fixture)")
-        }
-    }
-    
-    func test_ParseFixturesCompare() {
-        
     }
     
     func test_ParseBenchFixtures() {
-        // TODO: Need to implement
+        let bundle = Bundle(for: type(of: self))
+        let urls =
+            bundle.urls(forResourcesWithExtension: "ftl", subdirectory: "fixtures/benches")! +
+            bundle.urls(forResourcesWithExtension: "ftl", subdirectory: "fixtures/benches/contexts/browser")! +
+            bundle.urls(forResourcesWithExtension: "ftl", subdirectory: "fixtures/benches/contexts/preferences")!
+
+        for url in urls.sorted(by: { $0.path < $1.path }) {
+            Self.testFixture(ftlUrl: url)
+        }
+    }
+    
+    private static func testFixture(ftlUrl: URL) {
+        // Parse
+        
+        let ftlContent = try! String(contentsOf: ftlUrl, encoding: .utf8)
+        let parseResult = parse(source: ftlContent)
+        let ast: Resource
+        switch parseResult {
+        case .success(let resource):
+            ast = resource
+        case .failure(let err):
+            ast = err.resource
+        }
+
+        // Encode
+        
+        let ftlEncodedData = try! JSONEncoder().encode(ast)
+        let jsonEncodedData = try! Data(contentsOf: ftlUrl.deletingPathExtension().appendingPathExtension("json"))
+        
+        // Assert
+        
+        let actual = try! createJsonString(data: ftlEncodedData)
+        let expected = try! createJsonString(data: jsonEncodedData)
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    private static func createJsonString(data: Data) throws -> String {
+        let object = try JSONSerialization.jsonObject(with: data, options: .init())
+        let data = try JSONSerialization.data(withJSONObject: object, options: .prettyPrinted)
+        return String(data: data, encoding: .utf8)!
     }
 
 }
