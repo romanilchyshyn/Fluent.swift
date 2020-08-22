c16b84cb8fe90ebbcc0d11012044ab37274d5ab7
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-08-22 11:41:31 +0300

    Create FluentBundle framework.

---
+++ FluentBundle/FluentBundle.h
@@ -0,0 +1,19 @@
+//
+//  FluentBundle.h
+//  FluentBundle
+//
+//  Created by  Roman Ilchyshyn on 22.08.2020.
+//  Copyright © 2020  Roman Ilchyshyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluentBundle.
+FOUNDATION_EXPORT double FluentBundleVersionNumber;
+
+//! Project version string for FluentBundle.
+FOUNDATION_EXPORT const unsigned char FluentBundleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluentBundle/PublicHeader.h>
+
+
